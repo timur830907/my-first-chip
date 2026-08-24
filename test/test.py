ebe37e96e884b0b81dfb569295914243aaae280a
timur830907 <64460574+timur830907@users.noreply.github.com>
2026-08-24 15:27:36 +0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,40 +1,24 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
-
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+async def test_pwm(dut):
+    dut._log.info("Start PWM test")
+    
+    # Запуск тактового сигнала (10 МГц)
+    clock = Clock(dut.clk, 100, units="ns")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
+    # Начальный сброс
     dut.rst_n.value = 0
+    dut.ena.value = 1
+    dut.ui_in.value = 128 # Duty cycle 50% (128/256)
+    dut.uio_in.value = 0
+    
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
-
-    dut._log.info("Test project behavior")
-
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    
+    # Ждем 300 тактов для работы ШИМ
+    await ClockCycles(dut.clk, 300)
+    dut._log.info("PWM test completed successfully")
